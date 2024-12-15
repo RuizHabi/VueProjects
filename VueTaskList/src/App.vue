ad66ac655f651eebd22d69fe047cab4b7298a8fa
Javier Ruiz <ruizjavi070@gmail.com>
2024-12-15 21:57:42 +0900

    copied previous project

--- VueTaskList/src/App.vue
+++ VueTaskList/src/App.vue
@@ -1,47 +1,57 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+// with this import add ref to line 6 to 8 and this.status to status.value
+
+// export default {
+//   setup() {no longer needed because I can make it shorter by putting it on line 1
+const name = ref('Javier Ruiz');
+const status = ref('active');
+const tasks = ref(['Task One', 'Task Two', 'Task Three']);
+const newTask = ref('');
+
+const toggleStatus = () => {
+  if (status.value === 'active') {
+    status.value = 'pending';
+  } else if (status.value === 'pending') {
+    status.value = 'inactive';
+  } else {
+    status.value = 'active';
+  }
+};
+
+const addTask = () => {
+  if (newTask.value.trim() !== '') {
+    tasks.value.push(newTask.value);
+    newTask.value = '';
+  }
+};
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+};
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h1>{{ name }}</h1>
+  <p v-if="status ==='active'">User is active</p>
+  <p v-else-if="status === 'pending'"> User is pending</p>
+  <p v-else>User is inactive</p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for='(task, index) in tasks' :key="task">
+      <span>
+        {{ task }}
+      </span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
+  </ul>
+  <br/>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <button v-on:click="toggleStatus"> Change Status</button>
+
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
